83c46bb169b7b4ce50954cf8670192d80a3c6ca0
Tommy Vasquez <tommyvasquezr@gmail.com>
2020-08-07 06:46:30 -0500

    render the stars

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,25 +1,34 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import Star from './Star';
 
 class StarRating extends Component {
+    // 1. Initialize a 'rating' state
+    state = {
+        rating: 0,
+    };
 
-  // Initialize a 'rating' state
+    // 2. Write a function that returns 5 Star components
+    renderStarts = () => {
+        let stars = [];
+        let maxRating = 5;
 
+        for (let i = 0; i < maxRating; i++) {
+            stars.push(<Star key={i} />);
+        }
+        return stars;
+    };
 
-  // Write a function that returns 5 Star components
+    // 3. Write an event handler that updates the rating state.
+    // Pass the function to a Star component via props
 
-
-  // Write an event handler that updates the rating state.
-  // Pass the function to a Star component via props
-
-
-  render() {
-    return (
-      <ul className="course--stars">
-        {/* Render the Star components */}
-      </ul>
-    );
-  }
+    render() {
+        return (
+            <ul className="course--stars">
+                {/* Render the Star components */}
+                {this.renderStarts()}
+            </ul>
+        );
+    }
 }
 
 export default StarRating;
